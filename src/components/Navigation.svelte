fa2621dd659a533748f130b043e0f0b9e4cd567a
craigshoemaker <craig@craigshoemaker.net>
2020-10-13 15:31:25 -0400

    Simplify store logic

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,24 +1,16 @@
 <script>
-  import { onMount } from "svelte";
   import { scope } from "../stores/uiStore";
 
-  let scopeName = $scope;
   let scopes = ["daily", "weekly", "monthly", "yearly"];
   const handleClick = (newScope) => {
     scope.set(newScope);
   };
-
-  onMount(() =>
-    scope.subscribe((value) => {
-      scopeName = value;
-    })
-  );
 </script>
 
 <section class="nav-container">
-  {#each scopes as scope}
+  {#each scopes as scopeItem}
     <span
-      on:click={() => handleClick(scope)}
-      class={scope === scopeName ? 'selected' : ''}>{scope}</span>
+      on:click={() => handleClick(scopeItem)}
+      class={scopeItem === $scope ? 'selected' : ''}>{scopeItem}</span>
   {/each}
 </section>
