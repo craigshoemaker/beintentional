914e58ffac19242b5cda2a9c1cbe14f7de454b11
craigshoemaker <craig@craigshoemaker.net>
2020-10-13 07:43:43 -0400

    Add labels for checkboxes

--- src/components/Tasks.svelte
+++ src/components/Tasks.svelte
@@ -2,16 +2,21 @@
   import { onMount } from "svelte";
   import { tasksStore } from "../stores/tasksStore";
   import { scope } from "../stores/uiStore";
+  import getRandomNumber from "../utils/getRandomNumber";
 
   const converter = new showdown.Converter();
   let html = "";
 
   const renderTasks = () => {
     html = converter.makeHtml($tasksStore[$scope]);
-    html = html.replace(
-      /\<li\>/g,
-      '<li><input type="checkbox" class="inline-block mr-2">'
-    );
+    html = html.replace(/<li>(.*)?<\/li>/g, (match, group) => {
+      const id = getRandomNumber(1, 10000);
+      return `
+      <li>
+        <input id="${id}" type="checkbox" class="inline-block mr-2">
+        <label for="${id}" class="cursor-pointer">${group}</label>
+      </li>`;
+    });
   };
 
   onMount(() => scope.subscribe(renderTasks));
@@ -20,7 +25,7 @@
 
 <div class="p-4 pl-8">
   <h2 class="title-2 sm:text-4xl">Tasks</h2>
-  <div class={scope}>
+  <div>
     {@html html}
   </div>
 </div>
