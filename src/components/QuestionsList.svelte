9e898cdc6c489778cf71ac862625c70a8bb7a325
John Papa <john@johnpapa.net>
2020-10-14 16:49:09 -0400

    store is now a derived store with reactive filtered"

--- src/components/QuestionsList.svelte
+++ src/components/QuestionsList.svelte
@@ -1,22 +1,8 @@
 <script>
   import { stores } from '../stores';
   import Question from './Question.svelte';
-  import getRandomNumber from '../utils/getRandomNumber';
 
-  const { getFilteredQuestions, scope, questions } = stores;
-  // $: filtered = getFilteredQuestions();
-
-  let filtered = [];
-
-  $: {
-    let temp = $questions.filter((q) => q.categories.includes($scope));
-    const max = { length: 3 };
-    const indexes = Array.from(max, () => getRandomNumber(0, temp.length));
-    filtered = temp.filter((q, index) => indexes.includes(index));
-  }
-
-  // let filtered = [];
-
+  const { filtered } = stores;
   let show = {};
 
   if (!localStorage.settings) {
@@ -27,13 +13,6 @@
   } else {
     show = JSON.parse(localStorage.settings);
   }
-
-  // const getFilteredQuestions = () => {
-  //   filtered = $questions.filter((q) => q.categories.includes($scope));
-  //   const max = { length: 3 };
-  //   const indexes = Array.from(max, () => getRandomNumber(0, filtered.length));
-  //   filtered = filtered.filter((q, index) => indexes.includes(index));
-  // };
 
   const toggleShow = (name) => {
     show[name] = !show[name];
@@ -46,9 +25,14 @@
   <div class="text-center mt-3">
     <button class="button" on:click={() => toggleShow('description')} title="Show/hide descriptions">📄</button>
     <button class="button" on:click={() => toggleShow('textarea')} title="Show/hide response boxes">⌨</button>
-    <button class="button" on:click={getFilteredQuestions} title="Recycle questions">♻</button>
+    <button
+      class="button"
+      on:click={() => {
+        /* TODO: getFilteredQuestions*/
+      }}
+      title="Recycle questions">♻</button>
   </div>
-  {#each filtered as question}
+  {#each $filtered as question}
     <Question {question} {show} />
   {/each}
 </div>
--- src/stores/index.js
+++ src/stores/index.js
@@ -2,21 +2,32 @@ import { uiStore } from './uiStore.js';
 import { questionsStore } from './questionsStore.js';
 import { tasksStore } from './tasksStore.js';
 import getRandomNumber from '../utils/getRandomNumber';
-import { writable } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
 
-let filtered = writable([]);
+// let filtered = writable([]);
 
-function getFilteredQuestions() {
-  let temp = questionsStore.questions.filter((q) => q.categories.includes(uiStore.scope));
+// $: {
+//   let temp = questionsStore.questions.filter((q) => q.categories.includes(uiStore.scope));
+//   const max = { length: 3 };
+//   const indexes = Array.from(max, () => getRandomNumber(0, temp.length));
+//   filtered.set(temp.filter((q, index) => indexes.includes(index)));
+// }
+
+const { questions } = questionsStore;
+const { scope } = uiStore;
+
+function getFiltered(q, s) {
+  let temp = q.filter((q) => q.categories.includes(s));
   const max = { length: 3 };
   const indexes = Array.from(max, () => getRandomNumber(0, temp.length));
-  filtered.set(temp.filter((q, index) => indexes.includes(index)));
+  return temp.filter((q, index) => indexes.includes(index));
 }
 
+const filtered = derived([questions, scope], ([$q, $s]) => getFiltered($q, $s));
+
 export const stores = {
-  getFilteredQuestions,
-  filtered,
   ...uiStore,
   ...questionsStore,
   ...tasksStore,
+  filtered,
 };
