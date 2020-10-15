8f90e0718c13a6546886b691e85f9c7fed3b80de
craigshoemaker <craig@craigshoemaker.net>
2020-10-14 21:43:53 -0400

    Fix tasks

--- src/components/Tasks.svelte
+++ src/components/Tasks.svelte
@@ -1,32 +1,11 @@
 <script>
-  import { stores } from '../stores';
-  import getRandomNumber from '../utils/getRandomNumber';
-
-  let { scope, tasks } = stores;
-  const converter = new showdown.Converter();
-  let html = '';
-
-  const renderTasks = () => {
-    // TODO: Could you put this in the templatem and just call a function that gives you the random numbers?
-    html = converter.makeHtml($tasks[$scope]);
-    if (html && html.length) {
-      html = html.replace(/<li>(.*)?<\/li>/g, (match, group) => {
-        const id = getRandomNumber(1, 10000);
-        return `
-        <li>
-          <input id="${id}" type="checkbox" class="inline-block mr-2">
-          <label for="${id}" class="cursor-pointer">${group}</label>
-        </li>`;
-      });
-    }
-  };
-
-  renderTasks();
+  import { stores } from "../stores";
+  let { tasks } = stores;
 </script>
 
 <div class="p-4 pl-8">
   <h2 class="title-2 sm:text-4xl">Tasks</h2>
   <div>
-    {@html html}
+    {@html $tasks}
   </div>
 </div>
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,24 +1,47 @@
-import { uiStore } from './uiStore.js';
-import { questionsStore } from './questionsStore.js';
-import { tasksStore } from './tasksStore.js';
-import getRandomNumber from '../utils/getRandomNumber';
-import { derived } from 'svelte/store';
+import { uiStore } from "./uiStore.js";
+import { questionsStore } from "./questionsStore.js";
+import { tasksStore } from "./tasksStore.js";
+import getRandomNumber from "../utils/getRandomNumber";
+import { derived } from "svelte/store";
 
+const converter = new showdown.Converter();
 const { questions } = questionsStore;
 const { scope } = uiStore;
+const { tasks } = tasksStore;
 
 function getFiltered(q, s) {
-  let temp = q.filter((q) => q.categories.includes(s));
+  const questions = q.filter((q) => q.categories.includes(s));
   const max = { length: 3 };
-  const indexes = Array.from(max, () => getRandomNumber(0, temp.length));
-  return temp.filter((q, index) => indexes.includes(index));
+  const indexes = Array.from(max, () => getRandomNumber(0, questions.length));
+  return questions.filter((q, index) => indexes.includes(index));
 }
 
-const filteredQuestions = derived([questions, scope], ([$q, $s]) => getFiltered($q, $s));
+function getTasks(t, s) {
+  const markdown = t[s];
+  let html = converter.makeHtml(markdown);
+  if (html && html.length) {
+    html = html.replace(/<li>(.*)?<\/li>/g, (match, group) => {
+      const id = getRandomNumber(1, 10000);
+      return `
+        <li>
+          <input id="${id}" type="checkbox" class="inline-block mr-2">
+          <label for="${id}" class="cursor-pointer">${group}</label>
+        </li>`;
+    });
+  }
+  return html;
+}
+
+const scopedTasks = derived([tasks, scope], ([$t, $s]) => getTasks($t, $s));
+
+const filteredQuestions = derived([questions, scope], ([$q, $s]) =>
+  getFiltered($q, $s)
+);
 
 export const stores = {
   ...uiStore,
-  ...questionsStore,
-  ...tasksStore,
+  // ...questionsStore,
+  // ...tasksStore,
   filteredQuestions,
+  tasks: scopedTasks,
 };
